d1fcbd3b8cae29fb39ad321a5842bd4de8dc48bb
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-06 00:19:53 +0300

    feat: config imports and exports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userReducer from "./slices/userSlice.ts";
+import {RootState} from "../types/root.ts";
 
 export const store = configureStore({
     reducer: {
         userReducer,
     },
-})
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type { RootState };
