a6221c8a75fd8f6dbe3227f9f8a287f5161823a1
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-05 23:59:04 +0300

    feat: connect store to app

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
-
+import userReducer from "./slices/userSlice.ts";
 
 export const store = configureStore({
     reducer: {
-
+        userReducer,
     },
 })
