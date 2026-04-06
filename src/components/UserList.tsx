bad251f8bc2b99240328c8b0e83f1806e23a88d9
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-06 23:48:57 +0300

    feat: fill user array from remote server

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useAppSelector} from "../hooks/useAppSelector.ts";
+import {useAppDispatch} from "../hooks/useAppDispatch.ts";
+import {fetchUsersAction} from "../store/action-creators/user.ts";
 
 const UserList: React.FC = () => {
     const {users, loading, error} = useAppSelector(state => state.user);
 
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsersAction() as any);
+    }, []);
+
+    if (loading) {
+        return <h1>Загрузка, пожалуйста подождите...</h1>
+    }
+
+    if (error) {
+        return <h1 style={{color: 'red'}}>Произошла ошибка: {error}</h1>
+    }
     return (
         <div>
-            
+            {users.map(user =>
+                <div key={user.id}>{user.name}</div>
+            )}
         </div>
     );
 };
