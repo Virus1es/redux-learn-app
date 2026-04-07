e05059256815314e6363b158b1f8f325fa63dd31
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-07 22:31:23 +0300

    feat: use todo list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import UserList from "./components/UserList.tsx";
+import TodoList from "./components/TodoList.tsx";
 
 function App() {
 
     return (
         <div>
             <UserList/>
+            <hr/>
+            <TodoList/>
         </div>
     )
 }
