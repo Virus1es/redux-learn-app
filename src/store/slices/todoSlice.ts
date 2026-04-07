c5215d958b7b43a02e09d51e7cdb7e7883c0a72a
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-07 21:51:46 +0300

    create: create empty todo slice

---
+++ src/store/slices/todoSlice.ts
@@ -0,0 +1,19 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+const initialState: TodoState = {
+
+}
+
+const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+
+    },
+});
+
+export const {
+
+} = todoSlice.actions;
+export default todoSlice.reducer;
