1e37b6abb833294eb559828b68f17a0c8034c48c
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-07 22:00:56 +0300

    feat: add reducers and fields to slice

--- src/store/slices/todoSlice.ts
+++ src/store/slices/todoSlice.ts
@@ -1,15 +1,46 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import type {TodoState} from "../../types/todo.ts";
+import type {UserState} from "../../types/user.ts";
 
 
 const initialState: TodoState = {
-
+    todos: [],
+    page: 1,
+    error: null,
+    loading: false,
+    limit: 10,
 }
 
 const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-
+        fetchTodos(state: TodoState, action: PayloadAction<null>): TodoState {
+            return {
+                ...state,
+                loading: true,
+            };
+        },
+        fetchTodosSuccess(state: TodoState, action: PayloadAction<any[]>): TodoState {
+            return {
+                ...state,
+                loading: false,
+                todos: action.payload,
+            };
+        },
+        fetchTodosError(state: TodoState, action: PayloadAction<string>): TodoState {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        },
+        setTodoPage(state: TodoState, action: PayloadAction<number>): TodoState {
+            return {
+                ...state,
+                page: action.payload,
+            };
+        },
     },
 });
 
